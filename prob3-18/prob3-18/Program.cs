adcdb62bb592eef2d95038aa894b01b4dc4c65ae
bixynn2027 <59707557+bixynn2027@users.noreply.github.com>
2020-01-27 02:15:10 +0900

    期待されている実行結果の作成

--- prob3-18/prob3-18/Program.cs
+++ prob3-18/prob3-18/Program.cs
@@ -10,6 +10,28 @@ namespace prob3_18
     {
         static void Main(string[] args)
         {
+            Console.Write("温度を入力してください(‐10～35)：");
+            int a = int.Parse(Console.ReadLine());
+            if (a > -10 && a < 35)
+            {
+                Console.WriteLine("摂氏{0}度", a);
+                if (a >= 30)
+                {
+                    Console.WriteLine("真夏日です");
+                }
+                else if (a <= 0)
+                {
+                    Console.WriteLine("真冬日です");
+                }
+                else if (a >= 25 && a <= 30)
+                {
+                    Console.WriteLine("夏日です");
+                }
+            }
+            else
+            {
+                Console.WriteLine("適切な値を入力してください");
+            }
         }
     }
 }
